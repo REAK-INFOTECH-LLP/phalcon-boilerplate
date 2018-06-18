2489af79bdc998bdea79e0bae047533573ca0fea
Nilay <nilay.rawat@reak.in>
2018-06-18 13:56:42 +0530

    completed

--- project1/forgetredirect.php
+++ project1/forgetredirect.php
@@ -1,56 +1,35 @@
 <?php include 'includes/header.php'?>
-<div class="conatiner"></div>
+
+<div class="container">
     <div class="row">
         <div class="col-md-4"></div>
         <div class="col-md-4">
             <div class="card mt-5">
-            <div class="alert alert-dismissible alert-success" style="display:none" id="error">
-            <!-- <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <strong>Well done!</strong> You successfully read <a href="#" class="alert-link">this important alert message</a>. -->
-            </div>              
                 <div class="card-body">
-                <h4 class="card-header text-center">Make New Password</h4>
+                    <h4 class="card-header text-center">Forget Password</h4>
                     <p class="card-text">
-                     <form id="newpass" method="post">
-                    <div class="form-group">
-                          <label for=""></label>
-                          <input type="password" class="form-control" name="" id="new_password" aria-describedby="emailHelpId" placeholder="New Password">
-                         
-                        </div>
-                        <div class="form-group">
-                          <label for=""></label>
-                          <input type="password" class="form-control" name="" id="confirm_password"  placeholder="Confirm Password" onkeyup="Validate()">
-                        </div>
+                        <form>
+                            <div class="form-group">
+                                <label for=""></label>
+                                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="Enter Your Email Address"
+                                    required>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4"></div>
+                                <div class="col-md-4 text-center">
+                                    <button type="button" class="btn btn-outline-success">Submit</button>
+                                </div>
+                                <div class="col-md-4"></div>
+                            </div>
+                        </form>
                     </p>
-                    <div class="col-md-4"></div>
-                        <div class="row">
-                         <div class="col-md-4"></div>
-                            <div class="col-md-4 text-center">
-                            <button type="submit" name="submit" class="btn btn-outline-success ">Submit</button>
-                            </div> 
-                            <div class="col-md-4"></div>
-                        </div>
-                    </form>
-                    </div>
+
+                </div>
             </div>
+
         </div>
+        <div class="col-md-4"></div>
     </div>
 </div>
 
-
 <?php include 'includes/footer.php'?>
-
-
-<script type="text/javascript">
-  function Validate() {
-        var password = document.getElementById("new_password").value;
-        var confirmPassword = document.getElementById("confirm_password").value;
-        if (password != confirmPassword) {
-           document.getElementById('error').style.display = 'block'
-           document.getElementById('error').innerHTML="Password Not matched"
-        }else{
-           document.getElementById('error').innerHTML="matched"   
-        }
-        return ;
-    }
-</script>
--- project1/login.php
+++ project1/login.php
@@ -1,76 +1,81 @@
 <?php include 'includes/header.php'?>
 
-<div class="conatiner"></div>
-<div class="row">
-    <div class="col-md-4"></div>
+<div class="conatiner">
+    <div class="row">
+        <div class="col-md-4"></div>
 
-    <div class="col-md-4">
-        <div class="card mt-5">
-        <div class="alert alert-dismissible alert-success" style="display:none" id="error">
-         <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <strong>Well done!</strong> You successfully read <a href="#" class="alert-link">this important alert message</a>.
-        </div>
-            <div class="card-body">
-                <h4 class=" card-header card-title text-center">Log In</h4>
-                <p class="card-text">
-                    <div class="form-group">
-                        <label for=""></label>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="Email" onkeyup="validateEmail()" >
-                    </div>
-                    <div class="form-group">
-                        <label for=""></label>
-                        <input type="password" class="form-control" name="password" id="password"  placeholder="Password" required>
+        <div class="col-md-4">
+            <div class="card mt-5">
+                <!-- <div class="alert alert-dismissible alert-success" style="display:none" id="error">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>Well done!</strong> You successfully read
+                    <a href="#" class="alert-link">this important alert message</a>.
+                </div> -->
+                <div class="card-body">
+                    <h4 class=" card-header card-title text-center">Log In</h4>
+                    <!-- <p class="card-text"> -->
 
-                    </div>
-                </p>
-
-                <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="col-md-4 text-center">
-                        <button type="button" class="btn btn-outline-success">Submit</button>
-                    </div>
-                    <div class="col-md-4"></div>
-                </div>
-                <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-4">
+                        <form>
+                            <div class="form-group">
+                                <label for=""></label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email">
                             </div>
-                            <a href="forget_password.php">Forgot Password ? </a>
-                          </div>
+                            <div class="form-group">
+                                <label for=""></label>
+                                <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+
+                            </div>
+
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4 text-center">
+                            <button type="button" class="btn btn-outline-success" omsubmit="validateEmail()">Submit</button>
                         </div>
                         <div class="col-md-4"></div>
                     </div>
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                        </div>
+                        <a href="forget_password.php">Forgot Password ? </a>
+                    </div>
+                    <!-- </p> -->
+                </form>
+                </div>
+                <div class="col-md-4"></div>
             </div>
+           
         </div>
-
-
     </div>
 
-    <div class="col-md-4"></div>
+
+</div>
+
+<div class="col-md-4"></div>
 </div>
 </div>
 <?php include 'includes/footer.php'?>
 <script type="text/javascript">
 
+    // email & password
+
+    //  function validate(){
+    //     var password = document.getElementById("email").value;
+    //     var confirmPassword = document.getElementById("password").value;
+    //     if (email != password) {
+    //         document.getElementById('error').style.display = 'block'
+    //         document.getElementById('error').innerHTML = 'Email and Password are not matched'
+
+    //     }else{
+    //         document.getElementById('error')innerHTMl =  'Email and Password are matched'
+    //     }
+    //     return ;
+    // }
 
 
-//  function validate(){
-//     var password = document.getElementById("email").value;
-//     var confirmPassword = document.getElementById("password").value;
-//     if (email != password) {
-//         document.getElementById('error').style.display = 'block'
-//         document.getElementById('error').innerHTML = 'Email and Password are not matched'
-
-//     }else{
-//         document.getElementById('error')innerHTMl =  'Email and Password are matched'
-//     }
-//     return ;
-// }
-
-
-function validateEmail() {
-    var email = document.getElementById("email").value;
-        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    function validateEmail() {
+        var email = document.getElementById("email").value;
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; //  regex use is for email validation 
         if (reg.test(email.value) == false) 
         
         
