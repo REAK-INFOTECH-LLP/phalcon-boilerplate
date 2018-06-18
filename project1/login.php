f59fef7520ebab9eaf1084e5d6e26aac1746c0c5
Nilay <nilay.rawat@reak.in>
2018-06-18 14:39:44 +0530

    completed

--- project1/login.php
+++ project1/login.php
@@ -6,76 +6,53 @@
 
         <div class="col-md-4">
             <div class="card mt-5">
-                <!-- <div class="alert alert-dismissible alert-success" style="display:none" id="error">
-                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    <strong>Well done!</strong> You successfully read
-                    <a href="#" class="alert-link">this important alert message</a>.
-                </div> -->
+
                 <div class="card-body">
                     <h4 class=" card-header card-title text-center">Log In</h4>
-                    <!-- <p class="card-text"> -->
-
-                        <form>
-                            <div class="form-group">
-                                <label for=""></label>
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <label for=""></label>
-                                <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
-
-                            </div>
-
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-4 text-center">
-                            <button type="button" class="btn btn-outline-success" omsubmit="validateEmail()">Submit</button>
+                    <form action="" method="post" >
+                        <div class="form-group">
+                            <label for=""></label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Email">
                         </div>
-                        <div class="col-md-4"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-4">
+                        <div class="form-group">
+                            <label for=""></label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+
                         </div>
-                        <a href="forget_password.php">Forgot Password ? </a>
-                    </div>
-                    <!-- </p> -->
-                </form>
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4 text-center">
+                                <button type="button" class="btn btn-outline-success" onsubmit="validateEmail()">Submit</button>
+                            </div>
+                            <div class="col-md-4"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                            </div>
+                            <a href="forget_password.php">Forgot Password ? </a>
+                        </div>
+
+                    </form>
                 </div>
                 <div class="col-md-4"></div>
             </div>
-           
+
         </div>
     </div>
-
-
 </div>
-
 <div class="col-md-4"></div>
 </div>
 </div>
 <?php include 'includes/footer.php'?>
+
+
+<!-- java script for eamil validation -->
+
 <script type="text/javascript">
-
-    // email & password
-
-    //  function validate(){
-    //     var password = document.getElementById("email").value;
-    //     var confirmPassword = document.getElementById("password").value;
-    //     if (email != password) {
-    //         document.getElementById('error').style.display = 'block'
-    //         document.getElementById('error').innerHTML = 'Email and Password are not matched'
-
-    //     }else{
-    //         document.getElementById('error')innerHTMl =  'Email and Password are matched'
-    //     }
-    //     return ;
-    // }
-
-
     function validateEmail() {
         var email = document.getElementById("email").value;
-        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; //  regex use is for email validation 
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; // regex is a combination of special character and formate used for email validation(An email address has a format of type : An @ sign , a dot between address and domian and At least six characters)
         if (reg.test(email.value) == false) 
         
         
