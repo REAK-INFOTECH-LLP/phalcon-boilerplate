cc4696376a379fdf0720d20ccfa49892650002ab
Arjana kachhi <arjana.kachhi@reak.in>
2018-06-29 18:28:02 +0530

    logged-in bug fixed

--- app/app/controllers/IndexController.php
+++ app/app/controllers/IndexController.php
@@ -6,15 +6,6 @@ class IndexController extends ControllerBase
     public function indexAction()
     {
         $this->view->string = "You're at index/index";
-        /***
-         * If sessionVerify set true then redirect to dashboard
-         * otherwise go to website index page 
-         */ 
-        if($this->session->get("sessionVerify")== 1)
-        {
-            $this->response->redirect("index/dashboard");
-        }
-        
     }
     
     public function dashboardAction() {
